8d1c405581a34ee7f22e1883cc400f66cb8d1882
Administrator <Administrator@ADMINISTRATOR>
2017-05-22 16:57:27 +0800

    2017.5.22

---
+++ CaterDal/HallInfoDal.cs
@@ -0,0 +1,49 @@
+﻿using CaterModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaterDal
+{
+   public partial class HallInfoDal
+    {
+        public List<HallInfo> GetList()
+        {
+            string sql = "select * from HallInfo where hisdelete=0";
+            DataTable dt = SqliteHelper.GetDataTable(sql);
+            List<HallInfo> list = new List<HallInfo>();
+            foreach (DataRow row in dt.Rows)
+            {
+                list.Add(new HallInfo {
+                    HId=Convert.ToInt32(row["hid"]),
+                    HTitle=row["htitle"].ToString(),
+                });
+            }
+
+            return list;
+        }
+
+        public int Insert(HallInfo hi)
+        {
+            string sql = "insert into HallInfo(htitle,hisdelete) values(@title,0)";
+            SQLiteParameter p = new SQLiteParameter("@title", hi.HTitle);
+            return SqliteHelper.ExecuteNonQuery(sql, p);
+        }
+
+        public int Update(HallInfo hi)
+        {
+            string sql = "update HallInfo set htitle=@title where hid=@id";
+            SQLiteParameter[] sp =
+            {
+                new SQLiteParameter("@title",hi.HTitle),
+                new SQLiteParameter("@id",hi.HId)
+            };
+            return SqliteHelper.ExecuteNonQuery(sql, sp);
+        }
+       
+    }
+}
---
+++ CaterUI/FormHallInfo.cs
@@ -0,0 +1,66 @@
+﻿using CaterBll;
+using CaterModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaterUI
+{
+    public partial class FormHallInfo : Form
+    {
+        public FormHallInfo()
+        {
+            InitializeComponent();
+            hiBll = new HallInfoBll();
+        }
+
+        private HallInfoBll hiBll;
+        private void FormHallInfo_Load(object sender, EventArgs e)
+        {
+            LoadList();
+        }
+
+        private void LoadList()
+        {
+            dgvList.AutoGenerateColumns = false;
+            dgvList.DataSource = hiBll.GetList();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            HallInfo hi = new HallInfo()
+            {
+                HTitle = txtTitle.Text,
+            };
+
+            if (btnSave.Text=="添加")
+            {
+                if (hiBll.Insert(hi))
+                {
+                    LoadList();
+                }
+                else
+                {
+                    MessageBox.Show("error");
+                }
+            }
+
+            txtId.Text = "添加时无编号";
+            txtTitle.Text = "";
+            btnSave.Text = "添加";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtId.Text = "添加时无编号";
+            txtTitle.Text = "";
+            btnSave.Text = "添加";
+        }
+    }
+}
